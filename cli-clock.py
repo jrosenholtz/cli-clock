85bf0f5b210cb1e359e5d3e23d769a77c2b2fc72
jrosenholtz <josh@rosenholtz.net>
2022-10-01 18:35:28 -0500

    added conf file

--- cli-clock.py
+++ cli-clock.py
@@ -1,38 +1,49 @@
 #!/bin/python
 import pyfiglet
+import configparser
 from datetime import datetime
 from os import system, name
 import curses
 
-#screen = curses.initscr()
-#curses.noecho()
-#curses.cbreak()
+config = configparser.ConfigParser()
+config.read('cli-clock.conf')
+set_border = config.getboolean('Visuals', 'set_border')
 
 def timeUpdate():
     now =  datetime.now()
     return now.strftime("%H:%M:%S")
 
 def screenResize(y, x):
-    curses.resizeterm(y, x)
+    newY, newX = screen.getmaxyx()
+    if y != newY or x != newX:
+        curses.resizeterm(y, x)
+        screen.clear()
 
 def getTarget():
     currentTime = timeUpdate()
     drawTarget = pyfiglet.figlet_format(currentTime)
-    splitTarget=drawTarget.splitlines()
+    splitTarget = drawTarget.splitlines()
     return splitTarget
 
 def getDrawPos(y, x):
     target = getTarget()
     midX = int(x / 2)
-    midY = int(y / 2)
+    midY = int(y / 2) - int(len(target) / 2)
     midTarget = int(len(target) / 2)
-    midMess = int(len(str(target[midTarget])) / 2)
-    xPos = midX - midMess
+    middleMessage = int(len(target[midTarget]) / 2 )
+    xPos = midX - middleMessage 
     return midY, xPos
 
 def cursesInit():
     curses.curs_set(0)
+    curses.start_color()
     curses.use_default_colors()
+    for i in range(0, curses.COLORS):
+        curses.init_pair(i + 1, i, -1)
+
+def borders(screen):
+    if set_border == True:
+        screen.border()
 
 def drawTime(screen):
     cursesInit()
@@ -40,15 +51,12 @@ def drawTime(screen):
         y, x = screen.getmaxyx()
         target = getTarget()
         yPos, xPos = getDrawPos(y, x)
+        borders(screen)
         for num, line in enumerate(target):
-            screen.addstr(num + yPos, xPos, line)
+            screen.addstr(num + yPos, xPos, line, curses.color_pair(0))
         screen.refresh()
-        curses.napms(200)
+        curses.napms(100)
         screen.erase()
-        newY, newX = screen.getmaxyx()
-        if y != newY or x != newX:
-            screenResize(newY, newX)
-            screen.clear()
 
 def main(screen):
         drawTime(screen)
